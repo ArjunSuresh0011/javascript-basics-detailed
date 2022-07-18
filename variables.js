73c7dad97d06a4a27248694a6ef1bb915551639d
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-18 19:12:03 +0530

    variables, var keyword ,naming rules

---
+++ variables.js
@@ -0,0 +1,27 @@
+"use strict";
+
+// variable is used for storing the value or data .
+// variable is changeble
+
+// declaring a variable
+
+var firstName = "Arjun";
+var second_name = "Suresh";
+
+console.log(`${firstName} ${second_name} \n`);
+
+// if we dont declare the variable with var or any data type thire is no problem in js
+
+var fullname = "Sajitha suresh";
+
+console.log(fullname);
+
+// but programmers prefer use strict data type
+// if we add "use strict" on the the top of the code the variable must be declare with var or any data type
+
+// valid variable names
+
+var fullName  // camal case (first word start with small letter and second word start with uppercase without space)
+var full_name // using underscore insted of space
+var $fullName // $is used to name a variable
+
