b46916af643a4d58d299948419cb5a235d12b182
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-18 20:26:28 +0530

    variable declaration using let keyword

---
+++ let.js
@@ -0,0 +1,8 @@
+// let is used instd of var 
+
+let name = 'Arjun'
+console.log(name)
+
+// ones declared a variable name with let it not posible to declare the variable again
+
+// let name is showing error
