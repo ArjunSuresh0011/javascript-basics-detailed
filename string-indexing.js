a5f68f0bec69036e174596deb94a6d1d1fa150ab
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-18 20:45:22 +0530

    string indexing

---
+++ string-indexing.js
@@ -0,0 +1,26 @@
+// strings can represented using ""  or ''
+
+let sureName = 'Arjun suresh'
+let nickName = 'Appu'
+
+console.log(sureName);
+console.log(nickName);
+
+// find the length of a string
+
+console.log(`lenght of ${sureName} is `+ sureName.length)
+
+// the string indexing has start counting from 0
+
+console.log(sureName[0]);
+console.log(sureName[1]);
+console.log(sureName[2])
+console.log(sureName[3])
+console.log(sureName[4])
+console.log(sureName[5]);
+
+
+// to find last index 
+
+console.log(sureName[sureName.length-1]);
+
