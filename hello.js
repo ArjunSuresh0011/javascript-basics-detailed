3be75184553525fd8ab55330bde73dec9b83f2a0
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-18 18:28:51 +0530

    Hello world program

--- hello.js
+++ hello.js
@@ -1,1 +1,14 @@
-console.log('hello world ')
+// this is a basic helloworld program
+
+console.log('hello world ');
+
+// we can use '' single quots or dubble quots for writing strin
+
+console.log('hello Arjun');
+
+// And also we can use `` back tag for writing template literal
+
+console.log(`This is writen in
+back tack
+so while braking the sentence also
+affect the string `);
