b3d5292d39008f7b65470f0acb6b12c76ae84027
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-19 13:28:02 +0530

    if-else

---
+++ if-else.js
@@ -0,0 +1,30 @@
+// if else statement check the condition and return true or false based on the condition
+
+let color = 'red';
+
+if (color === 'red'){
+    console.log('Given color is RED');
+}else{
+    console.log('Given color is not RED');
+}
+
+
+
+// check even or odd number using if else
+
+let num = 7648;
+console.log('\n'+num +'\n');
+
+if(num%2===0){
+    console.log(`${num} is even`);
+}else{
+    console.log(`${num} is odd`);
+}
+
+let mark = 85;
+if(mark >80 && mark <=90){
+    console.log('AB');
+}
+else{
+    console.log('incorrect');
+}
