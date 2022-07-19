57a7949c263e18980a0df14536b3f98c7749786d
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-19 11:08:06 +0530

    string concatination

---
+++ string-concatination.js
@@ -0,0 +1,22 @@
+// concatination of string
+
+let empFirstName = 'surya'
+let empSecondName = 'vijay'
+
+// concatinating with + operator
+
+console.log(empFirstName+" "+empSecondName);
+
+
+// converting string to number and  use + operator
+
+let str1 = "10";
+let str2 = "20";
+
+let sum1 = str1+str2;
+console.log(sum1);
+
+// now converting string to number
+
+let sum2 = +str1 + +str2
+console.log(sum2);
