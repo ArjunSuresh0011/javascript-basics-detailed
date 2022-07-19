c0468cbd12fb7e7f5ec5549a7f3366b84fe2fc1c
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-19 11:55:02 +0530

    boolean and comparison operator

---
+++ booleans-comparison-operator.js
@@ -0,0 +1,25 @@
+// boolean
+
+let name = 'arjun'
+console.log(name === 'arjun')   //true or false
+
+// comparison operator
+console.log('\n=========================================\n');
+
+let num1 = 44
+let num2 =33
+let str1 = "44"
+
+console.log(num1 < num2);
+console.log(num1 > num2);
+console.log(num1 <= num2);
+console.log(num1 >= num2);
+console.log(num1 == str1);
+console.log(num1 === str1);
+console.log(num1 != str1);
+console.log(num1 !== str1);
+
+
+//  the  == operator not checking the type it check the value only
+// the === operator check the value and type
+
