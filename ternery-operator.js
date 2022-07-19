6ec24e1d021656f6cd843e9972f98f9a21b8ab18
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-19 13:42:33 +0530

    ternery operator

---
+++ ternery-operator.js
@@ -0,0 +1,14 @@
+// checking the given fruit color
+
+let fruit = 'orange';
+
+let color = fruit==='orange' ? 'orange color' : "This is not orange"
+console.log(color);
+
+
+// check which number is grater
+
+let num1 = 20;
+let num2 = 23;
+let max = num1 >= num2 ? `${num1} is gratest` : `${num2} is gratest`;
+console.log(max);
