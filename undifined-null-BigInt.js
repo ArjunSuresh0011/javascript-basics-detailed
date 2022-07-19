524ccffb58175c85dae73d057947fe4747627603
ArjunSuresh0011 <arjunsuresh0211@gmail.com>
2022-07-19 11:36:44 +0530

    undefind- null- BigInt

---
+++ undifined-null-BigInt.js
@@ -0,0 +1,24 @@
+// undefined
+
+let name;   // not assign any value it is undefined
+console.log(typeof name);
+
+// null
+
+let value = null;               // null means nothing
+console.log(typeof value);
+
+
+// BigInt
+
+//the number is gratest it be the power of 10 
+
+let num = 12345356365365434354766;
+console.log(num);
+console.log(typeof num);
+
+//so we need to cunvert the integer to BigInt
+
+let num2 = BigInt(num);
+console.log(num2);
+console.log(typeof num2);
